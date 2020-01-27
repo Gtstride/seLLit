5b5b3748d7f35d5e7750ef77766166318389edcf
Godstime <lewisagho47@gmail.com>
2020-01-27 15:05:45 +0100

    Integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
@@ -6,22 +6,47 @@ import HomePage from "./pages/homePage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
 
 import SignInSignUp from "./pages/auth/SignInSignUp";
+import { auth } from "./firebase/FireBase.utils";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/signin" component={SignInSignUp} />
-          <Route exact path="/shop" component={ShopPage} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() { 
+    this.unsubscriebeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header currentUser={this.state.currentUser} />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/signin" component={SignInSignUp} />
+            <Route exact path="/shop" component={ShopPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
